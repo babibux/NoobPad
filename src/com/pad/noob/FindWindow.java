f46d469e1003def92280e3b17922bf43ad5cd014
Barbara B <34717718+babibux@users.noreply.github.com>
2018-04-11 00:22:37 +0200

    Enhancing the Find functionnality

--- src/com/pad/noob/FindWindow.java
+++ src/com/pad/noob/FindWindow.java
@@ -2,15 +2,13 @@ package com.pad.noob;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
@@ -18,9 +16,10 @@ import javax.swing.text.Highlighter;
 import javax.swing.text.Highlighter.HighlightPainter;
 
 public class FindWindow {
-	JDialog frame = new JDialog();
-	JTextField textField = new JTextField(15);
-	JButton nextBtn = new JButton("Next");
+	private JDialog frame = new JDialog();
+	private JTextField textField = new JTextField(15);
+	private JButton nextBtn = new JButton("Next");
+	private int keepSearch = 0;
 	
 	public FindWindow(NoobPad noobPad) {
 		frame.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
@@ -36,33 +35,27 @@ public class FindWindow {
 		
 		Highlighter highlighter = noobPad.getTextArea().getHighlighter();
 	    HighlightPainter painter = new DefaultHighlighter.DefaultHighlightPainter(Color.lightGray);
-		
-	    /* 
-	    Still not 100% working :
-	    - the highlighted text isn't focused by the cursor
-	    - if there's more than one occurrence, the below method highlights all of them
-	    */
-		nextBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ev) {
+	      
+		nextBtn.addActionListener(ev-> {
 			    // removing previous highlight when clicking on Next button
 			    highlighter.removeAllHighlights();
 			    
 			    String userInput = new String(textField.getText());
 			    String textFile = new String(noobPad.getTextArea().getText());
-			    int indexStart = textFile.indexOf(userInput);
+			    int indexStart = textFile.indexOf(userInput, keepSearch);
 			    int indexEnd = indexStart + userInput.length();
 			    
-			    while(indexStart >= 0) { // indexOf returns -1 when no occurrences are found
+			    if(indexStart != -1) {
 			    	try {
-						highlighter.addHighlight(indexStart, indexEnd, painter);
-					} catch (BadLocationException e) {
-						e.printStackTrace();
-					}
-			    	//noobPad.getTextArea().setCaretPosition(indexStart);
-			    	indexStart = textFile.indexOf(userInput, indexStart+1);
-			    	indexEnd = indexStart + userInput.length();
+			    		highlighter.addHighlight(indexStart, indexEnd, painter);
+			    	} catch (BadLocationException e) {
+			    		e.printStackTrace();
+			    	}
 			    }
-			}
+			    else {
+			    	JOptionPane.showMessageDialog(frame, "End of the research session.");
+			    }
+			    keepSearch = indexEnd -1;
 		});
 		
 		// removing previous highlight when closing the Find window
